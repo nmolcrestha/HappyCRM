0b9636ccdd4a04fc65f4f357bd8a2b4cf6ddeaf6
Anmol <nmol.crestha@gmail.com>
2024-10-17 13:38:01 +0545

    Added middleware

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,14 @@
 
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\MessageController;
-use App\Models\Message;
-use App\Models\User;
+use App\Http\Middleware\BlockCurrentUser;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user/{user}/message', [MessageController::class, 'show']);
+Route::get('/user/{user}/message', [MessageController::class, 'show'])->middleware(BlockCurrentUser::class);
 Route::post('/user/{user}/message', [MessageController::class, 'submit'])->name('message.submit');
 
 Route::get('/user/{user}/appointment', [AppointmentController::class, 'show']);
